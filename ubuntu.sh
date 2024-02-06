c7505b888caaa413e31c4b1f8c37a8b45aef407d
Eric J. Suh <esuh@descript.com>
2024-02-05 21:01:37 -0800

    Make install tolerant of existing bashrc or gitconfig

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,6 +2,15 @@
 
 # Run ubuntu first, to override anything in posix
 stow --dotfiles -t "${HOME}" ubuntu
+
+if [ -f "${HOME}/.bashrc}" -a ! -h "${HOME}/.bashrc" ]; then
+  mv "${HOME}/.bashrc" "${HOME}/.bashrc_local"
+fi
+
+if [ -f "${HOME}/.gitconfig" -a ! -h "${HOME}/.gitconfig" ]; then
+  mv "${HOME}/.gitconfig" "${HOME}/.gitconfig_local"
+fi
+
 stow --dotfiles --defer=.* -t "${HOME}" posix
 stow --dotfiles --defer=.* -t "${HOME}/.config" posix-dot-config
 mkdir -p "${HOME}/.local/bin"
